01a7f77eafb4d81144c3647d64363c7b5da13a0d
James Pamplona <pamplonajames@gmail.com>
2015-08-23 13:37:57 -0700

    Initial Commit

---
+++ didSetTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  didSetTest
+//
+//  Created by James Pamplona on 8/23/15.
+//  Copyright © 2015 James Pamplona. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
